e6dd8df7f48092fe0b3fdd5e31e1b3a8deae6e90
RobsonMourasz <robsonic10@gmail.com>
2025-02-12 18:58:48 -0300

    CORRIGINDO CATEGORIA QUE NAO QUER APRESENTAR

--- htdocs/Request/Categoria/pesqCategoriaDespesa.php
+++ htdocs/Request/Categoria/pesqCategoriaDespesa.php
@@ -3,31 +3,38 @@ include_once __DIR__ . "/../../Data/conexao.php";
 if (!isset($_SESSION)) {
     session_start();
 }
-
-if(isset($_GET['id'])){
-    if($_GET['id'] === "todos"){
-        $PesqCat = $conexao->query("SELECT a.*, b.DescricaoSub,b.idSub
-        FROM cadcategoria a 
-        LEFT JOIN catsubcategoria b ON a.idCat = b.idCat
-        WHERE a.Tipo = 'D' ORDER BY a.DescricaoCat");
-        $PesqCat = $PesqCat->fetch_all(MYSQLI_ASSOC);
-        $retorno = array("Retorno" => "OK", "Dados" => $PesqCat);
-    }else{
-        $id= intval(limpar_texto($_GET['id']));
+    
+if (isset($_GET['id'])) {
+    
+    if ($_GET['id'] === "todos") {
+        
+        try {
+                $PesqCat = $conexao->query("SELECT a.*, b.DescricaoSub,b.idSub
+                    FROM cadcategoria a 
+                    LEFT JOIN catsubcategoria b ON a.idCat = b.idCat
+                    WHERE a.Tipo = 'D' ORDER BY a.DescricaoCat");
+                $PesqCat = $PesqCat->fetch_all(MYSQLI_ASSOC);
+                $retorno = array("Retorno" => "OK", "Dados" => $PesqCat);
+                
+        } catch (\Throwable $th) {
+            $retorno = array("Retorno" => "ERRO", "Dados" => $th->getMessage());
+        }
+    } else {
+        $id = intval(limpar_texto($_GET['id']));
         $PesqCat = $conexao->query("SELECT a.*, b.DescricaoSub, b.idSub
         FROM cadcategoria a 
         LEFT JOIN catsubcategoria b ON a.idCat = b.idCat 
         WHERE a.idCat =  $id");
-        if($PesqCat->num_rows > 0){
+        if ($PesqCat->num_rows > 0) {
             $PesqCat = $PesqCat->fetch_all(MYSQLI_ASSOC);
-            $retorno = array("Retorno" => "OK", "Dados" => $PesqCat);  
-        }else{
-            $retorno = array("Retorno" => "ERRO", "Motivo" => "Nenhuma Categoria Encontrada !");  
+            $retorno = array("Retorno" => "OK", "Dados" => $PesqCat);
+        } else {
+            $retorno = array("Retorno" => "ERRO", "Motivo" => "Nenhuma Categoria Encontrada !");
         }
     }
     echo json_encode($retorno);
     exit;
-}else{
+} else {
     $retorno = array("Retorno" => "ERRO", "Motivo" => "GET id não encontrado!");
     echo json_encode($retorno);
     exit;
